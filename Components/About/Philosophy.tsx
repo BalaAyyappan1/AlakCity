def047e0e19ec6ef607b937f2a18be3a635018f5
BalaAyyappan1 <balaayyappan11@gmail.com>
2026-04-23 12:13:48 +0530

    switching to astro

--- Components/About/Philosophy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Philosophy = () => {
-  return (
-    <div>Philosophy</div>
-  )
-}
-
-export default Philosophy
--- Components/About/Pillars.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Pillars = () => {
-  return (
-    <div>Pillars</div>
-  )
-}
-
-export default Pillars
