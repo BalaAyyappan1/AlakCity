e5eba513e6965f9e4e55d9e326259401dc847797
BalaAyyappan1 <balaayyappan11@gmail.com>
2026-04-23 14:44:42 +0530

    home, nav, footer done

---
+++ src/assets/home/index.ts
@@ -0,0 +1,13 @@
+export { default as heroImage } from "./hero.png";
+export {default as Accomadation} from './accomdation.png'
+export {default as EventsImage} from "./events.png";
+export {default as BarImage} from "./bar.png";
+export {default as DineImage} from "./dine.png";
+export {default as LuxuryImage} from "./luxury.png";
+export {default as Heart} from "./heart.png";
+export {default as FooterLogo} from "./logo.png";
+
+
+
+
+
